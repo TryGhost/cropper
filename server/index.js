1a8062174443e5558d2f7abfd00fc8b1e403832f
Fabien O'Carroll <fabien@allou.is>
2018-08-30 19:54:44 +0000

    correct path for public directory

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const createSharpMiddleware = require('./sharp-middleware');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/transform', createSharpMiddleware());
 
